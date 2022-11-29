78a8df7c9a342427d397cf79407c25b870662f78
minhduc0711 <phamminhduc0711@gmail.com>
2022-11-29 14:24:12 +0000

    Fix some errors in training script

--- train_seq2seq_decoder.py
+++ train_seq2seq_decoder.py
@@ -1,9 +1,14 @@
 import clip
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
 
 from src.models import Seq2SeqDecoder
 from src.data import DiffusionDBDataset
 
 clip_model, preprocess = clip.load("ViT-B/32")
+# For some reason, the weights in the CLIP model are automatically converted
+# to float16. We convert it back to float32
+clip_model = clip_model.float()
 
 ds = DiffusionDBDataset(img_transform=preprocess, subset_name="large_first_1k")
 train_dataloader = DataLoader(ds, batch_size=64, shuffle=True)
